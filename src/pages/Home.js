78e2512ded3e03ae3b107bba69887456e579d2f5
Mehmet Sait Işık <mehmet.isik@morhipo.com>
2022-03-16 00:27:32 +0300

    ilk commit

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import { react } from "react"
+import Layout from "../components/Layout";
+
+const Home = () => {
+    return (
+        <Layout>
+            <h1>
+                selam aleyke
+            </h1>
+        </Layout>
+    )
+}
+
+export default Home;
---
+++ src/pages/SignIn.js
@@ -0,0 +1,14 @@
+import { react } from "react"
+import Layout from "../components/Layout";
+import { supabase } from "../supabase";
+
+
+const SignIn = () => {
+    return (
+        <Layout>
+            selam aleyke signin
+        </Layout>
+    )
+}
+
+export default SignIn;
